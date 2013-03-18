44c2314374eb4a8668f2ff501e60c1a47b171e7a
kennx <zhangxjian@me.com>
2013-03-18 21:25:34 +0800

    first

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source  'https://www.rubygems.org'
+
+gem 'sinatra',                    :require => 'sinatra/base'
+gem 'sinatra-weixin-robot',       :require => 'sinatra/weixin-robot'
---
+++ app.rb
@@ -0,0 +1,30 @@
+# encoding: utf-8
+
+require 'rubygems'
+require 'bundler'
+Bundler.require
+
+class App < Sinatra::Base
+
+  register Sinatra::WeiXinRobot
+
+  configure do
+    enable  :logging
+    set     :weixin_token, "2646743010"
+    set     :weixin_uri,   "http://yaoyaohuahuapangpang.cloudfoundry.com/"
+  end
+
+  get "#{settings.weixin_path}" do
+    "#{params[:echostr]}"
+  end
+
+  post "#{settings.weixin_path}" do
+    if generate_signature == params[:signature]
+      msg = message_receiver(request.body)
+      weixin.sender(:msg_type => "text") do |msg|
+        msg.content = "Hello"
+      end
+    end
+  end
+
+end
